5b3d45650ca3cc300d180e0310fb87a6348aa213
AlejandroMarquino <90139066+AlejandroMarquino@users.noreply.github.com>
2021-12-16 17:16:40 +0100

    Add files via upload

---
+++ alejandromarquino_Tema3_Ejercicio3_JavaBasico/src/tema3/Ejercicio3/Main.java
@@ -0,0 +1,16 @@
+package tema3.Ejercicio3;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Alejandro ", "Alex ", "Sandra ", "Ripley"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print(nombres[i]);
+        }
+
+    }
+}
